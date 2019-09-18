5f8cdeacda18193b2479dd29f585c5629674910a
rsena9 <resena133@gmail.com>
2019-09-18 11:45:32 -0600

    Added operator and  first pass of javadoocs

---
+++ src/edu/cnm/deepdive/Operator.java
@@ -0,0 +1,44 @@
+package edu.cnm.deepdive;
+
+import java.util.Arrays;
+
+/**
+ * Enumerated values representing operator in a postfix (RPN) calculator. Each opreator has a token
+ * that is used to recognize the operatior in a string and to recogognize the operator in an input
+ * string and tor epresent the operator output dtring java doc comments
+ */
+public enum Operator {
+
+
+  ADD("+"),
+
+  SUBTRACT("-"),
+
+  MULTIPLY("*"),
+
+  DIVIDE("/"),
+
+  SQUARE_ROOT("sqrt"),
+
+  POWER("^"),
+
+  MODULO("%");
+
+  private String token;
+
+  Operator(String token) {
+    this.token = token;
+
+  }
+
+
+  public static void main(String[] args) {
+    System.out.println(Arrays.toString(Operator.values()));
+  }
+
+
+  @Override
+  public String toString() {
+    return token;
+  }
+}
