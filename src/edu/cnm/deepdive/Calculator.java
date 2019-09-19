525eab6fffbfcd2aebc0923b40f1892a7a81641d
rsena9 <resena133@gmail.com>
2019-09-19 09:20:57 -0600

    Use try with resources

--- src/edu/cnm/deepdive/Calculator.java
+++ src/edu/cnm/deepdive/Calculator.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive;
 
+import java.io.InputStream;
 import java.util.Deque;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
@@ -7,33 +8,31 @@ import java.util.LinkedList;
 
 public class Calculator {
 
-  private Scanner scanner;
-  private Deque<Double> operands;
 
   public static void main(String[] args) {
-    Calculator calc = new Calculator(new Scanner(System.in));
-    calc.process();
+    Calculator calc = new Calculator();
+    calc.process(System.in);
   }
 
-  public Calculator(Scanner scanner) {
-    this.scanner = scanner;
-    operands = new LinkedList<>();
-  }
-  public void process() {
-    try {
+  //REMOVE...operands = new LinkedList<>();
+
+  public void process(InputStream in) {
+    // in example throws comment above  here..throws NoSuchElementException
+    private Deque<Double> operands = new LinkedList<>();
+
+    try (Scanner scanner = new Scanner(in)) {
+
       while (scanner.hasNextDouble()) {
         operands.push(scanner.nextDouble());
       }
-
-  } catch(NoSuchElementException ignored) {
-
+    } catch (NoSuchElementException ignored) {
+      //end of input; complete processing
     } finally {
 
-    System.out.println(operands);
+      System.out.println(operands);
 
+    }
   }
-}
-
 }
 
 
